22a1e0e7fd49a0398e83a24ac65e805c9da553fd
Chris Emerson <github@mail.nosreme.org>
2017-12-02 11:26:33 +0000

    Add parse rows function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io;
 use std::io::{Read};
+use std::str::FromStr;
+use std::fmt::Debug;
 
 pub fn get_input(n: u32) -> io::Result<String> {
     let filename = format!("data/{}.txt", n);
@@ -10,10 +12,30 @@ pub fn get_input(n: u32) -> io::Result<String> {
     Ok(data)
 }
 
+pub fn parse_rows<T:FromStr+Debug>(data: &str) -> Vec<Vec<T>>
+   where <T as FromStr>::Err: Debug
+{
+    data.lines()
+        .map(|line| line.split_whitespace()
+                        .map(|s| s.parse().expect("Failed to parse"))
+                        .collect())
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn parse_rows_test()
+    {
+        assert_eq!(vec![vec![1,2,3],
+                        vec![4,5]],
+                   parse_rows("1 2 3\n4 5"));
     }
 }
