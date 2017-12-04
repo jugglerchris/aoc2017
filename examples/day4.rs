49870192870f54fcfef531b456986ca07fc736a7
Chris Emerson <github@mail.nosreme.org>
2017-12-04 07:42:04 +0000

    Solve part 2.

--- examples/day4.rs
+++ examples/day4.rs
@@ -2,6 +2,7 @@
 extern crate aoc2017;
 
 use std::collections::HashSet;
+use std::convert::From;
 
 fn is_valid(words: &[&str]) -> bool {
     let mut set = HashSet::new();
@@ -17,13 +18,21 @@ fn solve(data: &[Vec<&str>]) -> usize {
         .count()
 }
 
-/*
-fn solve2(data: &[Vec<u64>]) -> u64 {
-    data.iter()
-        .map(find_divisible)
-        .sum()
+fn is_valid2(words: &[&str]) -> bool {
+    let mut set = HashSet::new();
+    for word in words {
+        let mut v: Vec<u8> = From::from(*word);
+        v.sort();
+        set.insert(String::from_utf8(v).unwrap());
+    }
+    set.len() == words.len()
 }
-*/
+
+fn solve2(data: &[Vec<&str>]) -> usize {
+    data.iter()
+        .filter(|x| is_valid2(x))
+        .count()
+}
 
 fn main() {
     let input = aoc2017::get_input(4).unwrap();
@@ -35,9 +44,10 @@ fn main() {
 
     println!("Answer part 1: {}", solve(&data));
 
-    /*
-    assert_eq!(solve2(&aoc2017::parse_rows("5 9 2 8\n9 4 7 3\n3 8 6 5\n")), 9);
+    assert_eq!(is_valid2(&["abcde", "fghij"]), true);
+    assert_eq!(is_valid2(&["abcde", "xyz", "ecdab"]), false);
+    assert_eq!(is_valid2(&["iiii", "oiii", "ooii", "oooi", "oooo"]), true);
+    assert_eq!(is_valid2(&["oiii", "ioii", "iioi", "iiio"]), false);
 
     println!("Answer part 2: {}", solve2(&data));
-    */
 }
