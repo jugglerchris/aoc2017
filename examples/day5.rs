c096755fe327a5de071f90810f53309cfdd64ff2
Chris Emerson <github@mail.nosreme.org>
2017-12-05 08:22:19 +0000

    Solve part 2.

--- examples/day5.rs
+++ examples/day5.rs
@@ -17,10 +17,33 @@ fn solve(input: &str) -> isize {
     }
 }
 
+fn solve2(input: &str) -> isize {
+    let mut data = aoc2017::parse_lines::<isize>(input);
+    let mut count = 0;
+    let mut pc = 0;
+    loop {
+        let offset = data[pc as usize];
+        let newpc = pc + offset;
+
+        let newoffset = if offset >= 3 { offset-1 } else { offset+1 };
+
+        data[pc as usize] = newoffset;
+
+        count += 1;
+        if (newpc as usize) >= data.len() || newpc < 0 {
+            return count;
+        }
+        pc = newpc;
+    }
+}
 fn main() {
     let input = aoc2017::get_input(5).unwrap();
 
     assert_eq!(solve("0\n3\n0\n1\n-3\n"), 5);
 
     println!("Answer part 1: {}", solve(&input));
+
+    assert_eq!(solve2("0\n3\n0\n1\n-3\n"), 10);
+
+    println!("Answer part 2: {}", solve2(&input));
 }
