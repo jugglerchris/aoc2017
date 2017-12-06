1bb94bb0a5cd52c68320e98fae92570c53e4df8c
Chris Emerson <github@mail.nosreme.org>
2017-12-06 08:23:25 +0000

    Solve part 2.

--- examples/day6.rs
+++ examples/day6.rs
@@ -1,7 +1,7 @@
 #![feature(conservative_impl_trait)]
 extern crate aoc2017;
 
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn to_u64(pots: &[usize]) -> u64 {
     let mut result = 0;
@@ -12,16 +12,16 @@ fn to_u64(pots: &[usize]) -> u64 {
     return result;
 }
 
-fn solve(input: &str) -> isize {
+fn solve(input: &str) -> (usize,usize) {
     let mut data = aoc2017::parse_rows::<usize>(input);
     let data = &mut data[0];
 //    assert_eq!(data.len(), 16);
     let len = data.len();
     let mut count = 0;
-    let mut seen = HashSet::new();
+    let mut seen = HashMap::new();
     let mut as_u64 = to_u64(&data);
     loop {
-        seen.insert(as_u64);
+        seen.insert(as_u64, count);
         let mut max = 0;
         let mut max_pos = 0;
         for idx in 0..len {
@@ -36,8 +36,8 @@ fn solve(input: &str) -> isize {
         }
         count += 1;
         as_u64 = to_u64(&data);
-        if let Some(_) = seen.get(&as_u64) {
-            return count;
+        if let Some(lastcount) = seen.get(&as_u64) {
+            return (count, count-lastcount);
         }
     }
 }
@@ -45,13 +45,9 @@ fn solve(input: &str) -> isize {
 fn main() {
     let input = aoc2017::get_input(6).unwrap();
 
-    assert_eq!(solve("0 2 7 0"), 5);
+    assert_eq!(solve("0 2 7 0"), (5, 4));
 
-    println!("Answer part 1: {}", solve(&input));
-
-    /*
-    assert_eq!(solve2("0\n3\n0\n1\n-3\n"), 10);
-
-    println!("Answer part 2: {}", solve2(&input));
-    */
+    let (a1, a2) = solve(&input);
+    println!("Answer part 1: {}", a1);
+    println!("Answer part 2: {}", a2);
 }
