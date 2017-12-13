7081d56b99e2fb602571a80673e2f5033f241688
Chris Emerson <github@mail.nosreme.org>
2017-12-13 07:42:06 +0000

    Day 13 part 2 done.

--- examples/day13.rs
+++ examples/day13.rs
@@ -21,6 +21,28 @@ fn solve(input: &str) -> usize {
     severity
 }
 
+fn solve2(input: &str) -> usize {
+    let mut scanners: Vec<(usize, usize)> = Vec::new();
+
+    for line in input.lines() {
+        let mut parts = line.split(':');
+        let depth = parts.next().map(str::parse).unwrap().unwrap();
+        let range = parts.next().map(str::trim).map(str::parse).unwrap().unwrap();
+        scanners.push((depth, range));
+    }
+
+    let mut delay = 0;
+    loop {
+        if scanners.iter()
+                   .any(|&(depth, range)|  (depth+delay) % ((range - 1) * 2) == 0)
+        {
+            delay += 1;
+            continue;
+        }
+        return delay;
+    }
+}
+
 fn main() {
     let input = aoc2017::get_input(13).unwrap();
 
@@ -30,4 +52,11 @@ fn main() {
 6: 4"#), 24);
 
     println!("Answer: {:?}", solve(&input));
+
+    assert_eq!(solve2(r#"0: 3
+1: 2
+4: 4
+6: 4"#), 10);
+
+    println!("Answer: {:?}", solve2(&input));
 }
