d09700c6905e40145921c3466d76d663dee3b188
Chris Emerson <github@mail.nosreme.org>
2017-12-16 21:13:58 +0000

    Finish part 2.

--- examples/day16.rs
+++ examples/day16.rs
@@ -28,8 +28,6 @@ fn parse_moves(input: &str) -> Vec<Move> {
 fn get_permutations(moves: &[Move], num_dancers: usize) -> (Vec<usize>, Vec<usize>)
 {
     let mut dancers: Vec<usize> = (0..num_dancers).collect();
-    // positions is a mapping from dancer to position in dancers
-    let mut positions: Vec<usize> = (0..num_dancers).collect();
     // Track what dancers have been renamed (swapped by name)
     let mut renames: Vec<usize> = (0..num_dancers).collect();
     let mut offset = 0usize;  // The current first position
@@ -45,10 +43,6 @@ fn get_permutations(moves: &[Move], num_dancers: usize) -> (Vec<usize>, Vec<usiz
                 let pos1 = (a + offset) % num_dancers;
                 let pos2 = (b + offset) % num_dancers;
                 dancers.swap(pos1, pos2);
-                // Update the reverse mapping
-                let dancer1 = dancers[pos1];
-                let dancer2 = dancers[pos2];
-                positions.swap(dancer1, dancer2);
             },
             Partner(a, b) => {
                 let pos1 = renames.iter().position(|&x| x == a as usize).unwrap();
@@ -66,10 +60,6 @@ fn get_permutations(moves: &[Move], num_dancers: usize) -> (Vec<usize>, Vec<usiz
 }
 
 fn solve(input: &str, num_dancers: usize) -> String {
-    let mut dancers: Vec<usize> = (0..num_dancers).collect();
-    // positions is a mapping from dancer to position in dancers
-    let mut positions: Vec<usize> = (0..num_dancers).collect();
-    let mut offset = 0usize;  // The current first position
     let moves = parse_moves(input);
 
     let (pos_perm, name_perm) = get_permutations(&moves, num_dancers);
@@ -78,10 +68,56 @@ fn solve(input: &str, num_dancers: usize) -> String {
             .collect()
 }
 
+fn compose(perm1: &[usize], perm2: &[usize]) -> Vec<usize>
+{
+    perm2.iter()
+         .map(|&off| perm1[off])
+         .collect()
+}
+
+fn compose_name(perm1: &[usize], perm2: &[usize]) -> Vec<usize> {
+    // After perm1, dancer 0 ends up in perm1[0]
+    //              dancer 1 ends up in perm1[1]
+    //              ...
+    // 
+    // After perm2, dancer N ends up where perm2[N] ended up above:
+    //              => perm1[perm2[N]]
+    (0..(perm1.len()))
+        .map(|n| perm1[perm2[n]])
+        .collect()
+}
+
+fn solve2(input: &str, num_dancers: usize, mut iterations: usize) -> String {
+    let moves = parse_moves(input);
+
+    let (mut pos_perm, mut name_perm) = get_permutations(&moves, num_dancers);
+
+    let mut final_pos_perm: Vec<usize> = (0..num_dancers).collect();
+    let mut final_name_perm = final_pos_perm.clone();
+
+    while iterations != 0 {
+        if (iterations & 1) != 0 {
+            final_pos_perm = compose(&final_pos_perm, &pos_perm);
+            final_name_perm = compose_name(&final_name_perm, &name_perm);
+        }
+        pos_perm = compose(&pos_perm, &pos_perm);
+        name_perm = compose(&name_perm, &name_perm);
+        iterations = iterations >> 1;
+    }
+
+    final_pos_perm.iter()
+                   .map(|off| (final_name_perm[*off] as u8 + b'a') as char)
+                   .collect()
+}
+
 fn main() {
     let input = aoc2017::get_input(16).unwrap();
 
     assert_eq!(&solve("s1,x3/4,pe/b", 5), "baedc");
 
     println!("Answer: {:?}", solve(&input, 16));
+
+    assert_eq!(&solve2("s1,x3/4,pe/b", 5, 2), "ceadb");
+
+    println!("Answer: {:?}", solve2(&input, 16, 1000000000));
 }
